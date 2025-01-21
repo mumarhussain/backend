75cc29849992b606a857253a9f769da84bf2ef8d
mumarhussain <mumarhussain126@gmail.com>
2025-01-21 20:09:29 +0500

    create some utils functions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 // require("dotenv").config({});
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import app from "./app.js";
 
 
 dotenv.config({path:"./env"})
-connectDB();
+connectDB().then(()=>{
+    app.linster(process.env.PORT, ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+}).catch((err)=>{
+    console.log("Mongo Connection Fails !!",err);
+})
 
 
 
